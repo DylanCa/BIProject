f70163d4ae33f50d39f985c5a7cae27e0a9b9ba7
Prototype <dylan.Cattelan@gmail.com>
2018-07-03 17:53:55 +0200

    Add: Enums for test variables

---
+++ data/ENUMS.py
@@ -0,0 +1,27 @@
+test_candy = [
+    "Acidofilo", "Bouteille cola", "Brazil pik", "Color Schtroummpf pik",
+    "Langues acides", "London pik", "Miami pik", "Pasta Basta", "Pasta frutta",
+    "Sour snup", "Dragibus", "Carensac", "Fraizibus", "Grain de millet",
+    "Starmint", "Florent violette", "Kimono", "Pain Zan", "Rotella", "Zanoïd",
+    "Fraise tagada", "Croco", "Chamallows", "Polka", "Banane", "Ourson",
+    "Filament"
+]
+
+test_color = [
+    "Rouge", "Orange", "Jaune", "Vert", "Bleu", "Violet", "Noir", "Marron"
+]
+
+test_variant = ["Acide", "Sucré", "Gélifié"]
+
+test_texture = ["Mou", "Dur"]
+
+test_conditionning = ["Sachet", "Boite", "Echantillon"]
+
+test_countries = [
+    "Allemagne", "Autriche", "Belgique", "Bulgarie", "Chypre", "Croatie",
+    "Danemark", "Espagne", "Estonie", "Finlande", "France", "Grèce", "Hongrie",
+    "Irelande", "Italie", "Lettonie", "Lituanie", "Luxembourg", "Malte",
+    "Pays-Bas", "Pologne", "Portugal", "République tchèque", "Roumanie",
+    "Royaume-Uni", "Slovaquie", "Slovénie", "Suède", "USA", "Canada",
+    "Mexique", "Japon", "Chine", "Afrique du sud"
+]
