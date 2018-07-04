047f60dd708eeffe54a7e9349f2b4ae947c5b806
Prototype <dylan.Cattelan@gmail.com>
2018-07-04 18:29:36 +0200

    Refactor: orders.py to prepare the file for the upcoming methods

--- data/orders.py
+++ data/orders.py
@@ -3,16 +3,6 @@ import random
 from time import gmtime, strftime
 
 
-def generateOrder(clientName="UNKNOWN", nbOrder=0):
+def generateRandomOrders(clientName="UNKNOWN", clientSurname="UNKNOWN", nbOrder=0):
 
-    pool = list()
-
-    for x in range(0, nbOrder):
-        pool.append(
-            "Order #{} - Date: {}, Client: {}, Amount: {}, Total Cost: {}, Country: {}, Reference Candy: #{}".
-            format(x + 1, strftime("%d-%m-%Y %H:%M:%S", gmtime()), clientName,
-                   random.randint(1, 100), random.randint(1, 9999),
-                   random.choice(test_countries), random.randint(1, 3888)))
-
-    for line in pool:
-        print(line)
+    
