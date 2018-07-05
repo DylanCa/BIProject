a3e32175a3254a679be0a7b949e2d07596847dd9
Prototype <dylan.Cattelan@gmail.com>
2018-07-05 18:47:52 +0200

    Add: GenerateOrder now verifies references + creates an Order according

--- generator.py
+++ generator.py
@@ -1,9 +1,57 @@
+from data.tables import candies, packaging, countries, orders
+
+
 def generateRandomOrders(clientName="UNKNOWN",
                          clientSurname="UNKNOWN",
                          nbOrder=0):
     pass
-    
 
 
 def generateOrderPool(nbOrders=0):
     pass
+
+
+def generateOrder(clientName="UNKNOWN",
+                  clientSurname="UNKNOWN",
+                  clientMail="UNKNOWN",
+                  quantity=0,
+                  countryName="UNKNOWN",
+                  candyName="UNKNOWN",
+                  colorName="UNKNOWN",
+                  textureName="UNKNOWN",
+                  variantName="UNKNOWN",
+                  packagingName="UNKNOWN"):
+
+    packagingID = packaging.getPackagingByName(packagingName)[0][0]
+    countryID = countries.getCountryByName(countryName)[0][0]
+    candyPrice = candies.getCandyCostByID(candies.getCandyByName(candyName)[0][0])[0]
+
+    if packagingID == 2:
+        candyPrice = candyPrice[5]
+
+    elif packagingID == 3:
+        candyPrice = candyPrice[6]
+
+    elif packagingID == 4:
+        candyPrice = candyPrice[7]
+
+    totalCost = candyPrice * quantity
+
+    candyReferenceID = candies.createCandyReferenceByName(
+        candyName=candyName,
+        colorName=colorName,
+        textureName=textureName,
+        variantName=variantName,
+        packagingName=packagingName)[0][0]
+
+    orders.createOrder(
+        clientName=clientName,
+        clientSurname=clientSurname,
+        clientMail=clientMail,
+        quantity=quantity,
+        totalCost=totalCost,
+        orderState="PENDING",
+        countryID=countryID,
+        candyReference=candyReferenceID)
+
+    return True
