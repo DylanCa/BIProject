ac575c568131300847fc9263f183cf5e4bd9ad2e
Prototype <dylan.Cattelan@gmail.com>
2018-07-05 18:46:53 +0200

    Add: getVariantByName(name)

--- data/tables/variants.py
+++ data/tables/variants.py
@@ -2,7 +2,8 @@ from data.connection import oracle
 
 
 def getVariantByID(variantID=0):
-    cursor = oracle.connectToOracle()
+    connection = oracle.connectToOracle()
+    cursor = connection.cursor()
 
     cursor.execute(
         """
@@ -10,23 +11,35 @@ def getVariantByID(variantID=0):
      FROM VARIANT v
      WHERE (v.VARIANT_ID = :VARIANT_ID)""", {"VARIANT_ID": variantID})
 
-    for VARIANTNAME in cursor:
-        print(VARIANTNAME)
-
+    result = cursor.fetchall()
     cursor.close()
-    return
+    return result
+
+
+def getVariantByName(variantName=""):
+    connection = oracle.connectToOracle()
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+     SELECT v.VARIANT_ID, v.VARIANTNAME
+     FROM VARIANT v
+     WHERE (v.VARIANTNAME = :VARIANTNAME)""", {"VARIANTNAME": variantName})
+
+    result = cursor.fetchall()
+    cursor.close()
+    return result
 
 
 def getVariantsList():
-    cursor = oracle.connectToOracle()
+    connection = oracle.connectToOracle()
+    cursor = connection.cursor()
 
     cursor.execute("""
      SELECT v.VARIANTNAME
      FROM VARIANT v
     """)
 
-    for VARIANTNAME in cursor:
-        print(VARIANTNAME)
-
+    result = cursor.fetchall()
     cursor.close()
-    return
+    return result
