5e9ef774737cb5588d0c9982803379db7cc0200c
Prototype <dylan.Cattelan@gmail.com>
2018-07-05 12:22:42 +0200

    Refactor: removed now useless generator.py imports

--- generator.py
+++ generator.py
@@ -1,11 +1,3 @@
-import data.oracle as oracle
-import data.mongo as mongo
-import data.reference as reference
-
-import random
-from time import gmtime, strftime
-
-
 def generateRandomOrders(clientName="UNKNOWN",
                          clientSurname="UNKNOWN",
                          nbOrder=0):
