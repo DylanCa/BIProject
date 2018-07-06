87e97d24b05feefae4a1063bd10861f2519d2d45
Prototype <dylan.Cattelan@gmail.com>
2018-07-06 03:18:25 +0200

    Add: method to stock to update the Stock database

--- data/tables/stocks.py
+++ data/tables/stocks.py
@@ -1,4 +1,5 @@
 from data.connection import oracle
+from data.tables import candies
 
 
 def getStockByID(stockID=0):
@@ -51,3 +52,47 @@ def getStocksList():
     result = cursor.fetchall()
     cursor.close()
     return result
+
+
+def updateStockQuantityByCandyName(candyName="UNKNOWN"):
+
+    connection = oracle.connectToOracle()
+    cursor = connection.cursor()
+
+    results = candies.getCandyByName(candyName)[0]
+
+    quantities = []
+    quantities[0] = results[2]
+    quantities[1] = results[3]
+    quantities[2] = results[4]
+    quantities[3] = results[5]
+    quantities[4] = results[6]
+
+    component = []
+    component[0] = 'additive'
+    component[1] = 'coating'
+    component[2] = 'aroma'
+    component[3] = 'gelling'
+    component[4] = 'sugar'
+
+    for x in range(0, len(component)):
+        query = """UPDATE STOCK
+            SET PALETTEKG = ( SELECT PALETTEKG FROM STOCK WHERE COMPONENT = '{}') - {}
+            WHERE COMPONENT = '{}'""".format(component[x], quantities[x],
+                                             component[x])
+
+        cursor.execute(query)
+        connection.commit()
+
+
+def updateStockQuantityByID(stockID=0, quantity=0):
+
+    connection = oracle.connectToOracle()
+    cursor = connection.cursor()
+
+    query = """UPDATE STOCK
+        SET PALETTEKG = ( SELECT PALETTEKG FROM STOCK WHERE STOCK_ID = '{}') - {}
+        WHERE STOCK_ID = '{}'""".format(stockID, quantity, stockID)
+    cursor.execute(query)
+
+    connection.commit()
